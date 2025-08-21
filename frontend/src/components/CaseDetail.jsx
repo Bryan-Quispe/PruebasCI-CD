abcad63ccc0dd47590af2d71f9718d96b0dc8035
Bryan406 <rquisper406@gmail.com>
2025-08-21 00:54:15 -0500

    Pruebas y Lint corregidos CI/CD

--- frontend/src/components/CaseDetail.jsx
+++ frontend/src/components/CaseDetail.jsx
@@ -1,11 +1,7 @@
 function CaseDetail() {
   return (
     <div>
-      <h2>Detalle del caso</h2>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
+      <h2>caso</h2>
     </div>
   );
 }
